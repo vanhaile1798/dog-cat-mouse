a444a9a6db112cb75e106a2ce147a6836d313097
vanhaile1798 <vhail17.9.98@gmail.com>
2018-11-02 08:55:51 +0700

    Added two js files

---
+++ mouse.js
@@ -0,0 +1,11 @@
+function Mouse(name, color) {
+	this.name = name;
+	this.color = color;
+	this.speak = function() {
+		console.log('Hello Everyone');
+	}
+}
+
+let mickeyMouse = new Mouse('Mickey', 'black');
+
+mickeyMouse.speak();
